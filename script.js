8123f02731a5123ea77c3d46906287c785ebbc04
moddaman <ovrebo90@gmail.com>
2015-11-27 21:16:19 +0100

    dsadsa

--- script.js
+++ script.js
@@ -3,20 +3,25 @@
         $.getJSON("produkter.json", function(data) {
 
             items = [data.length];
-            var s = '';
-            var current = 0,
-                max = 100;
 
             for (var i = 0; i < data.length; i++) {
                 items[i] = data[i];
             }
             console.log('Vin size: ' + items.length);
-            for (var i = 0; i < max; i++) {
-                addToHtml(data[i]);
+            add100FirstRandomShitFuck();
 
-            }
 
         });
+
+        function add100FirstRandomShitFuck() {
+            var s = '';
+            var current = 0,
+                max = 100;
+            for (var i = 0; i < max; i++) {
+                addToHtml(items[i]);
+
+            }
+        }
 
         function addToHtml(v) {
             var s = '<article><h4>' + v.Varenavn + '</h4>';
@@ -32,31 +37,36 @@
             e.preventDefault();
         });
 
-         $('#searchInput').on('input', function() {
+        $('#searchInput').on('input', function() {
             // get the current value of the input field.
             console.log("change")
             search($("#searchInput").val());
-           
+
         });
 
 
 
         function search(query) {
-        	 $('#wines').empty();
+        	if(query.length==0){
+        		add100FirstRandomShitFuck();
+        		return;
+        	}
+
+            $('#wines').empty();
             console.log("searching for " + query);
-            var countResults=0;
+            var countResults = 0;
             for (var i = 0; i < items.length; i++) {
-                if (items[i].Varenavn.toLowerCase().indexOf(query.toLowerCase())>1) {
+                if (items[i].Varenavn.toLowerCase().indexOf(query.toLowerCase()) > 1) {
                     console.log("match :" + items[i]);
                     addToHtml(items[i]);
                     countResults++;
 
                 }
-                if(countResults>100){
-                	break;
+                if (countResults > 100) {
+                    break;
                 }
             }
-            console.log("results: "+countResults);
+            console.log("results: " + countResults);
 
         }
 
